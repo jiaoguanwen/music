73fb39b1d0b6cb23c0efcefed34b1bae6a6b34a4
jiaoguanwen <jiaoguanwen@163.com>
2017-12-03 15:11:32 +0800

    init proj

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="slider">
+    <div class="slider-group">
+      <slot>
+
+      </slot>
+    </div>
+    <div class="dots"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+</style>
---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="recommend">
+    <div class="recommend-content">
+      <div class="slider-wrapper">
+        <slider>
+          <div v-for="item in recommends">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl" alt="">
+            </a>
+          </div>
+        </slider>
+      </div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌曲推荐</h1>
+        <ul></ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import Slider from 'base/slider/slider'
+  import {getRecommend} from 'api/recommend'
+  import {ERR_OK} from 'api/config'
+
+  export default {
+    created() {
+      this._getRecommend()
+    },
+    data() {
+      return {
+        recommends: []
+      }
+    },
+    methods: {
+      _getRecommend() {
+        getRecommend().then((res) => {
+          if (ERR_OK === res.code) {
+            this.recommends = res.data.slider
+          }
+        })
+      }
+    },
+    components: {
+      Slider
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
