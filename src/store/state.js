775266b98fd055a94c3e19f7b608e37eeb60ad60
jiaoguanwen <jiaoguanwen@163.com>
2018-01-02 11:25:19 +0800

    * add rank page

--- src/store/state.js
+++ src/store/state.js
@@ -9,8 +9,8 @@ const state = {
   sequenceList: [],
   mode: playMode.sequence,
   currentIndex: -1,
-  disc: {}
-  // topList: {},
+  disc: {},
+  topList: {}
   // searchHistory: loadSearch(),
   // playHistory: loadPlay(),
   // favoriteList: loadFavorite()
