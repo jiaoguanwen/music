21e24870ec09def6d58435768e947579bdd25fd2
jiaoguanwen <jiaoguanwen@163.com>
2018-01-14 10:23:25 +0800

    add debounce func

--- src/base/search-box/search-box.vue
+++ src/base/search-box/search-box.vue
@@ -7,6 +7,8 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import {debounce} from 'common/js/util'
+
   export default {
     name: 'search-box',
     props: {
@@ -16,9 +18,9 @@
       }
     },
     created() {
-      this.$watch('query', (newQuery) => {
+      this.$watch('query', debounce((newQuery) => {
         this.$emit('query', newQuery)
-      })
+      }, 200))
     },
     data() {
       return {
